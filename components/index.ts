b2c0eec7fdb8ad9ee8910c9969d175be7b88743f
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2023-04-14 17:30:42 -0600

    feat(lib-a): secondary entry points

--- components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './src/lib/component-a';
-export * from './src/lib/component-b';
--- components/src/index.ts
+++ components/src/index.ts
@@ -1,5 +1,3 @@
-// export * from '@test/lib-a/components/src/lib/component-a';
-// export * from '@test/lib-a/components/src/lib/component-b';
+export * from '@test/lib-a/components/src/lib/component-a';
+export * from '@test/lib-a/components/src/lib/component-b';
 
-export * from './lib/component-a';
-export * from './lib/component-b';
