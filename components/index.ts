3d49e42fedd52c16035b266f5270752c6175a605
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2023-04-14 11:32:07 -0600

    fix(lib-a): config poly repo

---
+++ components/index.ts
@@ -0,0 +1,2 @@
+export * from './src/lib/component-a';
+export * from './src/lib/component-b';
--- src/lib/lib-a.module.ts
+++ src/lib/lib-a.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ComponentAModule } from '@test/lib-a/components/src/lib/component-a';
+import { ComponentAModule } from '@test/lib-a/components';
 
 @NgModule({
   imports: [CommonModule, ComponentAModule],
+  exports: [ComponentAModule]
 })
 export class LibAModule {}
